3586bcfd2dd757257fcc9be222862833f9930dde
Alex Perez <perez418@gmail.com>
2022-07-03 14:00:40 -0400

    card flip

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
+console.log('hello from main js')
 
+function randomNumberOneToFive(){
+    return Math.ceil(Math.random()*5)
+}
 
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const timestables = document.querySelector('input').value
+    //const timestables = document.querySelector('input').value
 
     try{
-        const response = await fetch(`https://mathapirockit.herokuapp.com/api`)
+        //const response = await fetch(`https://mathapirockit.herokuapp.com/api/${workout}`)
+        const response = await fetch(`https://mathapirockit.herokuapp.com/api/multiply`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data[0]
+        document.querySelector('h2').innerText = data[randomNumberOneToFive()-1]['factors']
     }catch(error){
     console.log(error)
 }
