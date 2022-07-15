4590698713ce566b03204282d67bcbc73b59bc63
Alex Perez <perez418@gmail.com>
2022-07-14 23:04:22 -0400

    card front displays factors

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 console.log('hello from main js')
 
+/* ***********Flip Card with JS************ */
+
+var cards = document.querySelectorAll('.card');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
+
+const frontCard = document.querySelector('.card__face--front');
+const cardButton = document.querySelector('.cardButton');
+const appTitle = document.querySelector('.appTitle');
+
+cardButton.addEventListener( 'click', apiRequest)
+
+// ***********************************************
+
 function randomNumberOneToFive(){
     return Math.ceil(Math.random()*5)
 }
-
-document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
     //const timestables = document.querySelector('input').value
@@ -14,8 +30,10 @@ async function apiRequest(){
         const response = await fetch(`https://mathapirockit.herokuapp.com/api/multiply`)
         const data = await response.json()
 
-        console.log(data)
-        document.querySelector('h2').innerText = data[randomNumberOneToFive()-1]['factors']
+        console.log(data[randomNumberOneToFive()-1])
+        document.querySelector('.card__face--front').innerText = data[randomNumberOneToFive()-1]['factors']
+
+        //return data[randomNumberOneToFive()-1]['factors']
     }catch(error){
     console.log(error)
 }
